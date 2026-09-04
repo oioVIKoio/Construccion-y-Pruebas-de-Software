61d9590366049b67dd0f4d4b81a5397f8dca63c9
Gonzalo Davila <davilagonzalo2304@gmail.com>
2026-09-03 22:42:18 -0500

    Modificar StudentRepository

--- semana03/cps_lab03/src/main/java/pe/edu/tecsup/lab03/repository/StudentRepository.java
+++ semana03/cps_lab03/src/main/java/pe/edu/tecsup/lab03/repository/StudentRepository.java
@@ -5,4 +5,20 @@ public class StudentRepository {
     public String obtenerNombre() {
         return "Gonzalo";
     }
+
+    public String obtenerApellido() {
+        return "Davila";
+    }
+
+    public int obtenerEdad() {
+        return 20;
+    }
+
+    public double obtenerNota1() {
+        return 15.0;
+    }
+
+    public double obtenerNota2() {
+        return 18.0;
+    }
 }
